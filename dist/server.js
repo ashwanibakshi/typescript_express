a32b967356889378d361bbaec9c4a9f144e08ae5
ashwanibakshi <ab324002@gmail.com>
2020-04-25 11:35:43 +0530

    first commit

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("./index");
-function start() {
-    var app = new index_1.App();
-    app.listen();
-}
-start();
